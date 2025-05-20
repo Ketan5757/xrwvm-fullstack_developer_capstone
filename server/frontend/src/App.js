ef8fd96c9fd2ed3d58d9fb548e35aef88282d6cf
Ketan <ketan.darekar99@gmail.com>
2025-05-20 18:18:37 +0200

    Dealership Feature added

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPanel />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dealers" element={<Dealers />} />
       </Routes>
     </Router>
   );
